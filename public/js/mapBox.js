38c394b1655fe9eb1500700c731526c9ca232a8c
Muhammad Gamal <moga1478@gmail.com>
2022-04-04 23:25:38 +0200

    Finsh map but still there some bug for content-Security-Policy

--- public/js/mapBox.js
+++ public/js/mapBox.js
@@ -7,5 +7,35 @@ mapboxgl.accessToken =
 
 var map = new mapboxgl.Map({
   container: 'map',
-  style: 'mapbox://styles/mapbox/streets-v11'
+  style: 'mapbox://styles/muga98/cl1k6yk2z004t15rjb03rqjxd',
+  scrollZoom: false
+  // center: [-118.113491, 34.111745],
+  // zoom:10,
+  // intersctive: false
+});
+const bounds = new mapboxgl.LngLatBounds();
+
+locations.forEach(loc => {
+  //create Marker
+  const el = document.createElement('div');
+  el.className = 'marker';
+  //Add Marker
+  new mapboxgl.Marker({ element: el, anchor: 'bottom' })
+    .setLngLat(loc.coordinates)
+    .addTo(map);
+
+  new mapboxgl.Popup({ offset: 30 })
+    .setLngLat(loc.coordinates)
+    .setHTML(`<p> ${loc.day}: ${loc.description}</p>`)
+    .addTo(map);
+  // extend map bounds to include current location
+  bounds.extend(loc.coordinates);
+});
+map.fitBounds(bounds, {
+  padding: {
+    top: 200,
+    bottom: 150,
+    left: 100,
+    right: 100
+  }
 });
