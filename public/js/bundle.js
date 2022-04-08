b62ce2bb050ce292e8427669aa69cc74ea720556
Muhammad Gamal <moga1478@gmail.com>
2022-04-08 03:21:50 +0200

    try to deploy on heroku change axios urls

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -8866,7 +8866,7 @@ var login = /*#__PURE__*/function () {
             _context.next = 4;
             return (0, _axios.default)({
               method: 'POST',
-              url: 'http://localhost:3000/api/v1/users/login',
+              url: '/api/v1/users/login',
               data: {
                 email: email,
                 password: password
@@ -8918,7 +8918,7 @@ var logout = /*#__PURE__*/function () {
             _context2.next = 3;
             return (0, _axios.default)({
               method: 'GET',
-              url: 'http://localhost:3000/api/v1/users/logout'
+              url: '/api/v1/users/logout'
             });
 
           case 3:
@@ -8971,7 +8971,7 @@ var updateSettings = /*#__PURE__*/function () {
       while (1) {
         switch (_context.prev = _context.next) {
           case 0:
-            url = type === 'password' ? 'http://localhost:3000/api/v1/users/updatemypassword' : 'http://localhost:3000/api/v1/users/updateMe';
+            url = type === 'password' ? '/api/v1/users/updatemypassword' : '/api/v1/users/updateMe';
             _context.prev = 1;
             _context.next = 4;
             return (0, _axios.default)({
@@ -9390,7 +9390,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60400" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61397" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
